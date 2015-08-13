1702ad52b6cea661069ee041c2d45f5b6924ff54
Taylor Monacelli <taylor.monacelli@streambox.com>
2015-08-12 20:03:51 -0700

    yada-yada

--- devdesktop.ps1
+++ devdesktop.ps1
@@ -1,3 +1,5 @@
+powershell -noprofile -executionpolicy unrestricted -command "(new-object System.Net.WebClient).DownloadFile('http://download.microsoft.com/download/2/E/6/2E61CFA4-993B-4DD4-91DA-3737CD5CD6E3/vcredist_x86.exe','vcredist_x86.exe')" && cmd /c vcredist_x86.exe /q
+
 & choco install vcredist2013 -yes -force
 & choco install mysql.workbench -yes -forcex86
 & choco install mysql.utilities -yes -forcex86
