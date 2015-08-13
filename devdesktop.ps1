58e8311441cf992d1da53fb29bf374c0e3f43e8d
Taylor Monacelli <taylor.monacelli@streambox.com>
2015-08-12 19:34:34 -0700

    yada-yada

--- devdesktop.ps1
+++ devdesktop.ps1
@@ -1,10 +1,12 @@
+& choco install vcredist2013 -yes -force
+& choco install mysql.workbench -yes -forcex86
+& choco install mysql.utilities -yes -forcex86
+
 & choco install 7zip -yes
 & choco install Firefox -yes
 & choco install GoogleChrome -yes
 & choco install cyberduck -yes
 & choco install dotnet3.5 -yes
-& choco install mysql.utilities -yes -forcex86
-& choco install mysql.workbench -yes -forcex86
 & choco install nmap -yes
 & choco install powershell4 -yes
 & choco install pscx -yes
