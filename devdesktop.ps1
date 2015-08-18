56f102fe11d115e5e2cea32b98dda02e12552f1a
Taylor Monacelli <taylor.monacelli@streambox.com>
2015-08-17 23:18:42 -0700

    Add Azure Powershell

--- devdesktop.ps1
+++ devdesktop.ps1
@@ -24,3 +24,4 @@ if((test-path 'vcredist_x86.exe') -and !(test-path '.vcredist_x86.exe'))
 & choco install sysinternals -yes
 & choco install vcredist2013 -yes -force
 & choco install windirstat -yes
+& choco install windowsazurepowershell -yes #must have PS 3 or higher
