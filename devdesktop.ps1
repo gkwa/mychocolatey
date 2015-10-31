b794cafcae3b2e552a69caca9d5b97c7765a6c44
Taylor Monacelli <taylor.monacelli@streambox.com>
2015-10-31 01:45:53 -0700

    Add wixtoolset

--- devdesktop.ps1
+++ devdesktop.ps1
@@ -40,3 +40,4 @@ if((test-path 'vcredist_x86.exe') -and !(test-path '.vcredist_x86.exe'))
 & choco install -yes windirstat
 & choco install -yes windowsazurepowershell #must have PS 3 or higher
 & choco install -yes wireshark
+& choco install -yes wixtoolset
