fc4b8bfa3fc4ca739d2fd7f3fdff328c83313bc6
Taylor Monacelli <taylor.monacelli@streambox.com>
2015-11-01 20:55:31 -0800

    Mount iso using imdisk
    Recommended by Chocolatey guys
    
    ImDisk Virtual Disk Driver (imdisk) is a software package that allows an
    ISO file to be mounted, but more importantly, it works for Windows
    NT/2000/XP/Vista/7/8/8.1 or Windows Server 2003/2008/2012 (so basically,
    everything!). That means that you can use one, common, method, for
    mounting ISO files when required within your Chocolatey Packages.
    
    https://goo.gl/3Mo8qM

--- devdesktop.ps1
+++ devdesktop.ps1
@@ -13,6 +13,7 @@ if((test-path 'vcredist_x86.exe') -and !(test-path '.vcredist_x86.exe'))
 & choco install -yes -forcex86 mysql.workbench
 & choco install -yes -forcex86 mysql.utilities
 
+& choco install -yes imdisk
 & choco install -yes chocolateypackageupdater
 & choco install -yes atom
 & choco install -yes keepass
