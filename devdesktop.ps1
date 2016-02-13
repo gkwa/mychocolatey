1b246399966240967ea2cfa730d1918fd6f252b6
Taylor Monacelli <taylor.monacelli@streambox.com>
2016-02-12 21:33:30 -0800

    Add Everything search engine
    Recomended for emacs helm from here:
    
    http://tuhdo.github.io/helm-intro.html
    
    Similar to helm-find, but use locate command and accepts search patterns
    according to locate input. Use helm-man-woman to read locate man
    page. In Mac OS, mdfind is used instead. On Windows, you need to install
    Everything search engine; once you installed Everything and make Emacs
    see the es.exe via PATH environment variable, helm-locate will use
    Everything and work out of the box without any configuration.

--- devdesktop.ps1
+++ devdesktop.ps1
@@ -8,6 +8,7 @@ if((test-path .\vcredist_x86.exe) -and !(test-path .\vcredist_x86.exe))
     1 | Out-File .\vcredist_x86.exe
 }
 
+choco install -yes everything
 choco install -yes nuget.commandline
 choco install -yes powershell4
 choco install -yes virtualbox
