19d6ca31a268b0ff981679c99427181a59c2da39
Taylor Monacelli <taylor.monacelli@streambox.com>
2016-08-17 08:43:10 -0700

    Add aws cli

--- devdesktop.ps1
+++ devdesktop.ps1
@@ -8,6 +8,7 @@ if((test-path .\vcredist_x86.exe) -and !(test-path .\vcredist_x86.exe))
     1 | Out-File .\vcredist_x86.exe
 }
 
+choco install -yes awscli
 choco install -yes realvnc
 choco install -yes vim
 choco install -yes visioviewer
